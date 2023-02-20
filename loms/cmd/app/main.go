00190802874ac83ef71d45189502a1ed042d209f
Denis Gavar <tskhelp@gmail.com>
2023-02-21 01:27:04 +0700

    [GOHW-1]: add loms orderpayed

--- loms/cmd/app/main.go
+++ loms/cmd/app/main.go
@@ -6,6 +6,7 @@ import (
 	"route256/libs/srvwrapper"
 	"route256/loms/internal/handlers/createorder"
 	"route256/loms/internal/handlers/listorder"
+	"route256/loms/internal/handlers/orderpayed"
 	"route256/loms/internal/handlers/stocks"
 )
 
@@ -15,10 +16,12 @@ func main() {
 	stocksHandler := stocks.New()
 	createOrderHandler := createorder.New()
 	listOrderHandler := listorder.New()
+	orderPayedHandler := orderpayed.New()
 
 	http.Handle("/stocks", srvwrapper.New(stocksHandler.Handle))
 	http.Handle("/createOrder", srvwrapper.New(createOrderHandler.Handle))
 	http.Handle("/listOrder", srvwrapper.New(listOrderHandler.Handle))
+	http.Handle("/orderPayed", srvwrapper.New(orderPayedHandler.Handle))
 
 	log.Println("listening http at", port)
 	err := http.ListenAndServe(port, nil)
