056109514009fcfa45c8a8ec8bd5532227fe31a5
Denis Gavar <tskhelp@gmail.com>
2023-02-21 00:20:48 +0700

    [GOHW-1]: add loms createorder

--- loms/cmd/app/main.go
+++ loms/cmd/app/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"route256/libs/srvwrapper"
+	"route256/loms/internal/handlers/createorder"
 	"route256/loms/internal/handlers/stocks"
 )
 
@@ -11,8 +12,10 @@ const port = ":8081"
 
 func main() {
 	stocksHandler := stocks.New()
+	createorder := createorder.New()
 
 	http.Handle("/stocks", srvwrapper.New(stocksHandler.Handle))
+	http.Handle("/createorder", srvwrapper.New(createorder.Handle))
 
 	log.Println("listening http at", port)
 	err := http.ListenAndServe(port, nil)
---
+++ loms/internal/handlers/createorder/createorder.go
@@ -0,0 +1,49 @@
+package createorder
+
+import (
+	"context"
+	"errors"
+	"log"
+)
+
+type Request struct {
+	User  int64  `json:"user"`
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	SKU   uint32 `json:"sku"`
+	Count uint16 `json:"count"`
+}
+
+var (
+	ErrEmptyUser  = errors.New("empty user")
+	ErrEmptyItems = errors.New("empty items")
+)
+
+func (r Request) Validate() error {
+	if r.User == 0 {
+		return ErrEmptyUser
+	}
+	if len(r.Items) == 0 {
+		return ErrEmptyItems
+	}
+	return nil
+}
+
+type Response struct {
+	OrderID int64 `json:"orderID"`
+}
+
+type Handler struct{}
+
+func New() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Handle(ctx context.Context, request Request) (Response, error) {
+	log.Printf("create order: %+v", request)
+	return Response{
+		OrderID: 42,
+	}, nil
+}
