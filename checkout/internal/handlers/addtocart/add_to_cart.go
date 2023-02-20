1e28ec49a1c22d8665bda5627e74bc59d4f06ee8
Denis Gavar <tskhelp@gmail.com>
2023-02-20 16:59:32 +0700

    [GOHW-1]: workshop

---
+++ checkout/internal/handlers/addtocart/add_to_cart.go
@@ -0,0 +1,50 @@
+package addtocart
+
+import (
+	"context"
+	"errors"
+	"log"
+	"route256/checkout/internal/domain"
+)
+
+type Handler struct {
+	businessLogic *domain.Model
+}
+
+func New(businessLogic *domain.Model) *Handler {
+	return &Handler{
+		businessLogic: businessLogic,
+	}
+}
+
+type Request struct {
+	User  int64  `json:"user"`
+	Sku   uint32 `json:"sku"`
+	Count uint16 `json:"count"`
+}
+
+type Response struct{}
+
+var (
+	ErrEmptyUser = errors.New("empty user")
+)
+
+func (r Request) Validate() error {
+	if r.User == 0 {
+		return ErrEmptyUser
+	}
+	return nil
+}
+
+func (h *Handler) Handle(ctx context.Context, req Request) (Response, error) {
+	log.Printf("addToCart: %+v", req)
+
+	var response Response
+
+	err := h.businessLogic.AddToCart(ctx, req.User, req.Sku, req.Count)
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
--- loms/cmd/app/main.go
+++ loms/cmd/app/main.go
@@ -1,7 +1,20 @@
 package main
 
-// const port = ":8081"
+import (
+	"log"
+	"net/http"
+	"route256/libs/srvwrapper"
+	"route256/loms/internal/handlers/stocks"
+)
+
+const port = ":8081"
 
 func main() {
+	stocksHandler := stocks.New()
 
+	http.Handle("/stocks", srvwrapper.New(stocksHandler.Handle))
+
+	log.Println("listening http at", port)
+	err := http.ListenAndServe(port, nil)
+	log.Fatal("cannot listen http", err)
 }
