ac4a416feddebe4b7eaab8cb7cd8fd0a88a809c0
Denis Gavar <tskhelp@gmail.com>
2023-04-15 14:34:22 +0700

    [GOHW-8]: added cache buckets

--- libs/cache/cache.go
+++ libs/cache/cache.go
@@ -7,6 +7,13 @@ import (
 	"time"
 )
 
+const (
+	// константы для хэш-функции
+	// https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function#FNV-1a_hash
+	fnvOffSetBasis uint64 = 14695981039346656037
+	fnvPrime       uint64 = 1099511628211
+)
+
 type Cache interface {
 	Set(ctx context.Context, key string, value interface{}) bool
 	Get(ctx context.Context, key string) interface{}
@@ -20,31 +27,43 @@ type cacheItem struct {
 	ttl *time.Time
 }
 
+type bucket struct {
+	// ключ - значение
+	data map[string]*list.Element
+	// для thread safe
+	mu *sync.Mutex
+}
+
 // кэш
 type cache struct {
 	// ёмкость кэша
 	capacity int
-	// ключ - значение
-	data map[string]*list.Element
+	// слайс бакетов
+	buckets []*bucket
 	//очередь для LRU
 	queue *list.List
 	// тикер для очистки кэша по ttl
 	ticker *time.Ticker
 	// ttl по умолчанию
 	ttl time.Duration
-	// для thread safe
-	mu *sync.Mutex
 }
 
 // конструктор
 func NewCache(capacity int, ttl time.Duration) Cache {
+	buckets := make([]*bucket, 16)
+	for index := range buckets {
+		buckets[index] = &bucket{
+			data: make(map[string]*list.Element),
+			mu:   &sync.Mutex{},
+		}
+	}
+
 	cache := &cache{
 		capacity: capacity,
-		data:     make(map[string]*list.Element),
+		buckets:  buckets,
 		queue:    list.New(),
 		ticker:   time.NewTicker(time.Second * 10),
 		ttl:      ttl,
-		mu:       &sync.Mutex{},
 	}
 
 	// запускаем горутину для очистки кэша по ttl
@@ -53,14 +72,27 @@ func NewCache(capacity int, ttl time.Duration) Cache {
 	return cache
 }
 
+// хэш-функция для определения номера бакета
+func hashValue(key string, limit int) int {
+	hash := fnvOffSetBasis
+	for _, b := range []byte(key) {
+		hash ^= uint64(b)
+		hash *= fnvPrime
+	}
+	return int(hash % uint64(limit))
+}
+
 // записываем значение в кэш
 func (c *cache) Set(ctx context.Context, key string, value interface{}) bool {
+	// определяем индекс бакета
+	hash := hashValue(key, len(c.buckets))
+
 	// блокировка
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.buckets[hash].mu.Lock()
+	defer c.buckets[hash].mu.Unlock()
 
 	// проверяем, есть ли уже значение по этому ключу
-	if val, ok := c.data[key]; ok {
+	if val, ok := c.buckets[hash].data[key]; ok {
 		// если значение есть
 		// перемещаем значение в начало очереди
 		c.queue.MoveToFront(val)
@@ -86,19 +118,22 @@ func (c *cache) Set(ctx context.Context, key string, value interface{}) bool {
 
 	// записываем элемент в начало очереди
 	el := c.queue.PushFront(item)
-	c.data[item.key] = el
+	c.buckets[hash].data[item.key] = el
 
 	return true
 }
 
 // получаем значение из кэша
 func (c *cache) Get(ctx context.Context, key string) interface{} {
+	// определяем индекс бакета
+	hash := hashValue(key, len(c.buckets))
+
 	// блокировка
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.buckets[hash].mu.Lock()
+	defer c.buckets[hash].mu.Unlock()
 
 	// пытаемся прочитать значение по ключу
-	if el, ok := c.data[key]; ok {
+	if el, ok := c.buckets[hash].data[key]; ok {
 		// если прочитали успешно
 		// перемещаем значение в начало очереди
 		c.queue.MoveToFront(el)
@@ -120,20 +155,25 @@ func (c *cache) deleteOldest() {
 func (c *cache) cleanCache() {
 	for {
 		<-c.ticker.C
-		// блокировка
-		c.mu.Lock()
-		// итерируемся по всем элементам
-		for _, val := range c.data {
-			// приводим элемент к нашему типу
-			el := val.Value.(*cacheItem)
-			if el.ttl != nil {
-				// если срок жизни элемента истёк, то удаляем
-				if time.Now().After(*el.ttl) {
-					c.deleteCacheItem(val)
+
+		// итерируемся по всем бакетам
+		for _, bucket := range c.buckets {
+			// блокировка
+			bucket.mu.Lock()
+			// итерируемся по всем элементам
+			for _, val := range bucket.data {
+				// приводим элемент к нашему типу
+				el := val.Value.(*cacheItem)
+				if el.ttl != nil {
+					// если срок жизни элемента истёк, то удаляем
+					if time.Now().After(*el.ttl) {
+						c.deleteCacheItem(val)
+					}
 				}
 			}
+			bucket.mu.Unlock()
 		}
-		c.mu.Unlock()
+
 	}
 }
 
@@ -141,6 +181,8 @@ func (c *cache) cleanCache() {
 func (c *cache) deleteCacheItem(el *list.Element) {
 	// удаляем из очереди
 	item := c.queue.Remove(el).(*cacheItem)
+	// определяем индекс бакета
+	hash := hashValue(item.key, len(c.buckets))
 	// удалем из map
-	delete(c.data, item.key)
+	delete(c.buckets[hash].data, item.key)
 }
